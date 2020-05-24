c32c79de14ba7509b712724ed154d4cbcaa79fea
Jianzengzhi Liao <liaojianzengzhi@hotmail.com>
2020-05-24 19:25:57 +0800

    一个用户model，可以存放用户的个人信息

---
+++ app/src/main/java/com/madderate/skyloft/models/UserInformation.kt
@@ -0,0 +1,9 @@
+package com.madderate.skyloft.models
+
+class UserInformation(
+    var userName: String = "",
+    var userPhoneNumber: String = "",
+    var userPhoneCountryCode: String = "",
+    var userEmailAddress: String = "",
+    var userPassword: String = ""
+)
